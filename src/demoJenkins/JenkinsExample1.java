05445dad6ae23f357a9a1e03ed940a5754e5dc70
unknown <nagesh1417@gmail.com>
2018-03-17 19:29:33 +0530

    Test

---
+++ src/demoJenkins/JenkinsExample1.java
@@ -0,0 +1,32 @@
+package demoJenkins;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class JenkinsExample1 {
+  @Test
+  public void f() {
+	  System.out.println("Welcome to Jenkins");
+	  System.out.println("This is my First Jenkins script");System.setProperty("webdriver.chrome.driver","F:\\Selenium downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+	  WebDriver driver=new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	  driver.get("https://www.facebook.com/");
+	  System.out.println("Page title:"+driver.getTitle());
+	  driver.close();
+  }
+  /*@BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }*/
+
+}
