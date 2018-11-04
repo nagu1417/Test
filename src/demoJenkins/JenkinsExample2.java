b3f642a286efb852aa3423f190de61963e7c85fd
Nagesh <nagesh1417@gmail.com>
2018-11-04 07:37:03 +0530

    Updating Test Case.

---
+++ src/demoJenkins/JenkinsExample2.java
@@ -0,0 +1,31 @@
+package demoJenkins;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterTest;
+
+public class JenkinsExample2 {
+  @Test
+  public void f() {
+	  System.out.println("Welcome to Jenkins");
+	  System.out.println("This is my Second Jenkins script");
+	  System.setProperty("webdriver.chrome.driver","F:\\Selenium downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+	  
+  }
+  /*@BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }*/
+
+}
